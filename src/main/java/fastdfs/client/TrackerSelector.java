4b93050d1f01539b72d69c701ce79dd18b12e98b
renzuoli <xxxxxxxxxxxxxx>
2018-05-29 16:26:23 +0800

    修改使用ThreadLocalRandom来获得随机数

--- src/main/java/fastdfs/client/TrackerSelector.java
+++ src/main/java/fastdfs/client/TrackerSelector.java
@@ -2,11 +2,23 @@ package fastdfs.client;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-
+/**
+ * 选择tracker策略
+ */
 public enum TrackerSelector {
 
     ROUND_ROBIN {
+        private final ThreadLocalIndex threadLocalIndex = new ThreadLocalIndex();
+
+        @Override
+        public TrackerServer select(List<TrackerServer> list) {
+            return list.get(threadLocalIndex.getAndIncrement(list.size()));
+        }
+
+    },
+    ROUND_ROBIN1 {
         private int idx;
 
         @Override
@@ -14,10 +26,9 @@ public enum TrackerSelector {
             idx %= list.size();
             return list.get(idx++);
         }
-
     },
     RANDOM {
-        private final Random random = new Random();
+        private final ThreadLocalRandom random = ThreadLocalRandom.current();
 
         @Override
         public TrackerServer select(List<TrackerServer> list) {
