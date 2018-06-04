aece363c5d5418546b6d572c575e2e84fd449753
renzuoli <xxxxxxxxxxxxxx>
2018-06-04 17:27:53 +0800

    解密FileId

--- src/test/java/fastdfs/client/T2.java
+++ src/test/java/fastdfs/client/T2.java
@@ -2,35 +2,26 @@ package fastdfs.client;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import sun.misc.BASE64Decoder;
 
-/**
- *
- * 项目名称：  fastdfs-client
- * 类名称：  T2
- * 描述：  
- * @author 李仁佐
- * 创建时间：  2018年05月29日
- * 修改人：李仁佐    修改日期： 2018年05月29日
- * 修改备注：
- *
- */
+
 public class T2 {
     private static final ThreadLocalRandom random = ThreadLocalRandom.current();
     private static final Random random1 = new Random();
     static final BASE64Decoder decoder = new BASE64Decoder();
+    static final SimpleDateFormat simpleDateFormat =new SimpleDateFormat("yyyy-MM-dd HH:mm:SS");
     public static void main(String[] args) {
         try {
-            ByteBuffer buffer = ByteBuffer.wrap(decoder.decodeBuffer("rBLkCVrQeYOAF3MrAAETwQfZhuY511"));
-            byte[] ip = new byte[4];
-            System.out.println(buffer.capacity());
+            ByteBuffer buffer = ByteBuffer.wrap(decoder.decodeBuffer("rBMDJ1sVBbmAYW6gAAKGUJ6TR0Y583"));
             System.out.println(buffer.position());
             int bbb = buffer.getInt();
             System.out.println(ipToIPv4Str(T9.toLH(bbb)));
-
-            buffer.get(ip,0,4);
+            Long times = Long.valueOf(buffer.getInt())*1000;
+            System.out.println(simpleDateFormat.format(new Date(times)));
 
         } catch (IOException e) {
             e.printStackTrace();
